a4b96bcd1c820bc50d632d4d2263e8283203b458
blairprophett <blaircuny@gmail.com>
2014-05-06 18:02:43 -0700

    all SF parks seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Park.create(name: 'Park 1 - Buena Vista Park', address: 'Haight St & Buena Vista E', leash_rule: 'off-leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
-
-Park.create(name: 'Park 2', address: 'some address', leash_rule: 'on/off leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
-
-Park.create(name: 'Park 3', address: 'some address', leash_rule: 'on/off leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
-
-Park.create(name: 'Park 4', address: 'some address', leash_rule: 'on/off leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
-
-Park.create(name: 'Park 5', address: 'some address', leash_rule: 'on/off leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
-
-Park.create(name: 'Park 6', address: 'some address', leash_rule: 'on/off leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
+Park.create(name: 'McLaren Park', address: 'Shelly Dr. & Mansell St.', leash_rule: 'Off Leash', image: 'http://i.imgur.com/pQEfOjSs.jpg')
+Park.create(name: 'Bernal Heights', address: 'Bernal Heights & Esmeralda', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'St. Mary’s Park', address: 'Murray Ave. & Justin Dr.', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Crissy Field', address: 'Mason Street', leash_rule: 'Designated off lease area ', image: '')
+Park.create(name: 'Ocean Beach', address: 'Great Highway at Sloat', leash_rule: '
+Off leash Stairwell 1 to 21', image: '')
+Park.create(name: 'Duboce Park', address: 'Duboce Ave and Noe St.', leash_rule: 'Designated off lease area', image: '')
+Park.create(name: 'Corona Heights', address: 'Randall Field at Museum Way & Roosevelt Way', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Glen Canyon Park', address: 'Bosworth St. & Diamond Heights Blvd.', leash_rule: 'Leashes Required', image: '')
+Park.create(name: 'Buena Vista Park', address: 'Buena Vista West and Central St.', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Fort Funston', address: 'Skyline & Fort Funston', leash_rule: 'Leashes required', image: '')
+Park.create(name: 'Lake Merced', address: 'Lake Merced Blvd.', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Lafayette Park', address: 'Lower Pacific Heights', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Dolores Park', address: '18th St. between Dolores & Church', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Upper Noe Park', address: 'Day St. & Sanchez', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Alta Plaza Park', address: 'Clay between Scott & Steiner on 2nd Terrace.', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'McKinley Square', address: '20th St. & Vermont', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Potrero Hill Mini Park', address: '22nd St. & Arkansas', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Mountain Lake Park', address: '12th Ave. & Lake', leash_rule: 'Off leash in east end of park', image: '')
+Park.create(name: 'Stern Grove', address: '19th Ave. & Wawona', leash_rule: 'Off Leash', image: 'Off leash in north side of park')
+Park.create(name: 'Rincon Hill Dog Park', address: '486 Beale St.', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Mission Creek Dog Park', address: '485 Berry St', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Walter Haas Park and Playground', address: 'Diamond Heights Blvd', leash_rule: 'Off Leash', image: '')
+Park.create(name: 'Golden Gate Park', address: 'From Stanyan St. to Great Hwy, between Lincoln Way and Fulton St.', leash_rule: 'Off Leash in mentioned areas', image: '')
 
 
 Dog.create(name: 'Dog 1- Riva', breed: 'Golden Retriever', age: '1 year, 6 months', photo: 'Dogs01.jpg', park_id: 1)
